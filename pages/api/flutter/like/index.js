818b7c7bad2657ca44a0b7722277e3f58c32792b
arno756 <arno756@users.noreply.github.com>
2023-02-26 20:25:35 -0800

    commit

--- pages/api/flutter/like/index.js
+++ pages/api/flutter/like/index.js
@@ -19,7 +19,6 @@ export default withApiAuthRequired(async function handler(req, res) {
 
   try {
     switch (req.method) {
-<<<<<<< Updated upstream
       case "PUT":
         const updateData = await fetch(`${baseUrl}/updateOne`, {
           ...fetchOptions,
@@ -36,22 +35,6 @@ export default withApiAuthRequired(async function handler(req, res) {
         const updateDataJson = await updateData.json();
         res.status(200).json(updateDataJson);
         break;
-=======
-    case "PUT":
-    const updateData = await fetch(`${baseUrl}/updateOne`, {
-    ...fetchOptions,
-    body: JSON.stringify({
-      ...fetchBody,
-      filter: { _id: { "$oid": req.body._id } },
-      update: { [req.body.action]: {
-        likes: req.body.userId,
-      } },
-    }),
-  });
-  const updateDataJson = await updateData.json();
-  res.status(200).json(updateDataJson);
-  break;
->>>>>>> Stashed changes
       default:
         res.status(405).end();
         break;
